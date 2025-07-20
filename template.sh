3e647d6be27d4289102c0c091625fc0744b1ccca
Eaze-dev <kgsotso16@gmail.com>
2025-07-20 14:49:15 +0200

    Folder structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+##Creating directory
+mkdir -p src
+mkdir -p research
+
+##Create files
+touch src/_init_.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynp
+touch requirements.txt
+touch README.md
+
+echo "Directory and files created successfully!"
